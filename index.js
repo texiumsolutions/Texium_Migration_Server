5dbe9e70f0749abc3ff4472f41511235ec5e19be
sheikhsumaya <sheikhsumaya622@gmail.com>
2023-03-06 23:20:34 +0600

    failed to connect

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb'); 
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+const app = express();
+
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster.mb6jarb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Database connected!')
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', ( req, res)=>{
+  res.send('Running Server!');
+});
+
+app.listen(port, ()=>{
+  console.log('Listening to Port',port)
+})
