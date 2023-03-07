b561d790cafb525e6c7a0a2cd1f9b21a17cdfe6a
sheikhsumaya <sheikhsumaya622@gmail.com>
2023-03-07 17:06:08 +0600

    after database connected

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const { response } = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 
-
-// Middleware 
 app.use(cors());
 app.use(express.json());
 
