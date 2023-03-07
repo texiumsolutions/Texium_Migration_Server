89ec7579136a79ae5b65e40d31a24fa6c964f1eb
NH-Shohan <nh.shohan6@gmail.com>
2023-03-07 13:30:22 +0600

    database connected happily

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,23 +9,37 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://texium_solution_migration:6Q2XCyWLPAmYkwfF@cluster.mb6jarb.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://texium_migration:CF4SLRt0yX0mKKha@cluster.mb6jarb.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("shohan ami asi to!");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const database = client.db("Texium_Migration");
+    const users = database.collection("user");
+    const fileCollection = database.collection("sourceFileInfo");
+
+    app.get("/sourceFileInfo", async (request, response) => {
+      const query = {};
+      const cursor = fileCollection.find(query);
+      const fileInfo = await cursor.toArray();
+      response.send(fileInfo);
+    });
+
+    console.log("Connected Before You Asked!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (request, response) => {
   response.send("Running All The Time");
 });
 
 app.listen(port, () => {
-  console.log("I'm Walking!");
+  console.log(`I'm Walking in ${port} number street!`);
 });
