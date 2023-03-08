73b9ee5d8971623a1d5514c98b50483702ea8d9f
sheikhsumaya <sheikhsumaya622@gmail.com>
2023-03-08 18:07:43 +0600

    crashed

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const cors = require("cors");
-const { response } = require("express");
+const fs = require("fs");
+const xlsx = require("xlsx");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const upload = multer({
+  dest: "./uploads/",
+  fileFilter: (req, file, cb) => {
+    const allowedExtensions = [".xlsx", ".csv"];
+    const ext = path.extname(file.originalname);
+    if (allowedExtensions.includes(ext)) {
+      cb(null, true);
+    } else {
+      cb(
+        new Error(
+          "Only Excel files with the .xlsx & .csv extension are allowed!"
+        )
+      );
+    }
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -19,16 +39,78 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const database = client.db("Texium_Migration");
-    const user = database.collection("user");
+    const users = database.collection("user");
     const fileCollection = database.collection("sourceFileInfo");
 
-    app.get("/sourceFileInfo", async (request, response) => {
-      const query = {};
-      const cursor = fileCollection.find(query);
-      const fileInfo = await cursor.toArray();
-      response.send(fileInfo);
+    app.use("/static", express.static("uploads"));
+
+    // File Upload
+    app.post("/uploadFile", upload.single("avater"), (request, response) => {
+      let newFileName = Date.now() + ".csv";
+
+      fs.rename(
+        `./uploads/${request.file.filename}`,
+        `./uploads/${newFileName}`,
+        function () {
+          // Read the Excel file and convert it to CSV format
+          const workbook = xlsx.readFile(`./uploads/${newFileName}`);
+          const sheetName = workbook.SheetNames[0];
+          const csv = xlsx.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+
+          // Write the CSV data to a new file
+          const csvFileName = newFileName.replace(".xlsx", ".csv");
+          fs.writeFile(`./uploads/${csvFileName}`, csv, (err) => {
+            if (err) {
+              console.log(err);
+              response.status(500).json({
+                success: false,
+                message: "Unable to convert the file.",
+              });
+            } else {
+              // Read the CSV data and add it to the database
+              fs.readFile(`./uploads/${csvFileName}`, (err, data) => {
+                if (err) {
+                  console.log(err);
+                  response.status(500).json({
+                    success: false,
+                    message: "Unable to read the file.",
+                  });
+                } else {
+                  const fileId = new Date().getTime().toString();
+
+                  const fileData = {
+                    _id: fileId,
+                    fileName: request.file.originalname,
+                    newFileName: csvFileName,
+                    filePath: `./uploads/${csvFileName}`,
+                    createdAt: new Date(),
+                    data: xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]),
+                  };
+                  // Add the file information and data to the database
+                  fileCollection.insertOne(fileData, function (err, res) {
+                    if (err) {
+                      console.log(err);
+                      response.status(500).json({
+                        success: false,
+                        message: "Unable to add file to the database.",
+                      });
+                    } else {
+                      response.status(200).json({
+                        success: true,
+                        message: "File uploaded successfully!",
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      );
     });
-    app.get("/user", async (request, response) => {
+
+    // Get all the data of files
+    app.get("/sourceFileInfo", async (request, response) => {
       const query = {};
       const cursor = fileCollection.find(query);
       const fileInfo = await cursor.toArray();
@@ -41,6 +123,15 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+// Default Error
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send(err.message);
+  } else {
+    res.send("Uploaded Successfully!");
+  }
+});
 
 app.get("/", (request, response) => {
   response.send("Running All The Time");
